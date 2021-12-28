d865220467bcf1f2dd394f87f370d4e79f7f61c4
Satyam54 <61054835+Satyam54@users.noreply.github.com>
2021-12-28 21:42:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 st.title("MY FIRST CLOUD APP")
 st.write("Streamlit cloud app")
 st.write("Hello")
-number1= st.slider("Pick any number",0,100)
-number2= st.slider("Pick any number",0,100)
+number1= st.slider("Pick some random no:",0,100)
+number2= st.slider("Pick some other random no:",0,100)
 x= number1+number2
 y= number1-number2
 st.write("SUM")
